25128072eb5f2dba7a321edfe3cbc9df102e8f6e
Keshat <keshat2004@gmail.com>
2025-08-20 11:51:20 +0530

    Update CameraUnstable.lua

--- scripts/CameraUnstable.lua
+++ scripts/CameraUnstable.lua
@@ -10,6 +10,7 @@ local camera = workspace.CurrentCamera
 -- edit 2 19 august
 -- edit 3 19 august
 -- edit 4 19 august
+-- edit 1 20 august
 player.CameraMode = Enum.CameraMode.LockFirstPerson
 
 -- Camera sway/bob parameterss
